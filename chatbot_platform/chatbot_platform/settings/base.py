b59894178b66e49d481178d54a1ed1d357d59fbc
BK <safvanbakkar041@gmail.com>
2025-07-01 16:27:38 +0530

    static path issue resolved

--- chatbot_platform/chatbot_platform/settings/base.py
+++ chatbot_platform/chatbot_platform/settings/base.py
@@ -70,7 +70,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "core/static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [BASE_DIR / "core/static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
