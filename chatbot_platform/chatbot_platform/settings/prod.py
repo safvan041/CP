d8ec2ca4f97207a1b35c4feaea58e3abcacfcaf5
BAKKAR SAFAWAN <127715574+safvan041@users.noreply.github.com>
2025-07-24 05:18:53 +0000

    allow host prepare for render

--- chatbot_platform/chatbot_platform/settings/prod.py
+++ chatbot_platform/chatbot_platform/settings/prod.py
@@ -5,6 +5,7 @@ ALLOWED_HOSTS = [
     'chatbot-api-platform-29773676777.us-central1.run.app',
     'localhost',
     '127.0.0.1',
+    'capi-studio.onrender.com',
 ]
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
